211c9e277912906400309d03696415a340c72375
Shayan Hajhashemi <shayan.hajhashemi@mail.mcgill.ca>
2023-11-26 01:17:22 -0500

    poised for finding ic50 fractions and effect trajectories

--- scripts/prototype/ic50_effect.jl
+++ scripts/prototype/ic50_effect.jl
@@ -2,3 +2,4 @@
 # or instead try many different amounts and see where it ends up with realtion to ic50
 # plot the effect amount as the constant dopes as a fraction of the ic50 is given on the schedule
 
+#change the order the equations are computed int he pkpd
